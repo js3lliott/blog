c6b76899d991db765aac3f3f74f131f50ca75856
Jordan Samek <jordan.samek@gmail.com>
2023-05-16 21:56:19 -0400

    Display modifications

--- components/Footer.js
+++ components/Footer.js
@@ -1,14 +1,14 @@
 import { currentDayName } from '@/lib/utils/dateUtils'
 import Link from './Link'
-import NowPlayingFooter from './NowPlayingFooter'
+// import NowPlayingFooter from './NowPlayingFooter'
 
 export default function Footer() {
   return (
     <footer>
       <div className="mt-10 flex flex-col items-center">
-        <div className="">
+        {/* <div className="">
           <NowPlayingFooter />
-        </div>
+        </div> */}
         <div className="mb-2 hidden text-sm text-gray-500 dark:text-gray-400 md:flex">
           <div className="mx-1">
             <Link href="https://jsamek.vercel.app" className="link-underline">
--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -17,34 +17,34 @@ const siteMetadata = {
   linkedin: 'https://www.linkedin.com/in/jordan-samek/',
   website: 'https://jsamek.vercel.app',
   locale: 'en-US',
-  analytics: {
-    plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
-    simpleAnalytics: false, // true or false
-    umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
-    googleAnalyticsId: '268418262', // e.g. UA-000000-2 or G-XXXXXXX
-  },
-  newsletter: {
-    provider: 'emailOctopus',
-  },
-  comment: {
-    provider: 'giscus',
-    giscusConfig: {
-      repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
-      repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
-      category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
-      categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
-      mapping: 'pathname',
-      reactions: '1',
-      metadata: '0',
-      // theme example: light, dark, dark_dimmed, dark_high_contrast
-      // transparent_dark, preferred_color_scheme, custom
-      theme: 'light',
-      inputPosition: 'bottom',
-      lang: 'en',
-      darkTheme: 'dark',
-      themeURL: '',
-    },
-  },
+  // analytics: {
+  //   plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
+  //   simpleAnalytics: false, // true or false
+  //   umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
+  //   googleAnalyticsId: '268418262', // e.g. UA-000000-2 or G-XXXXXXX
+  // },
+  // newsletter: {
+  //   provider: 'emailOctopus',
+  // },
+  // comment: {
+  //   provider: 'giscus',
+  //   giscusConfig: {
+  //     repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
+  //     repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
+  //     category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
+  //     categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
+  //     mapping: 'pathname',
+  //     reactions: '1',
+  //     metadata: '0',
+  //     // theme example: light, dark, dark_dimmed, dark_high_contrast
+  //     // transparent_dark, preferred_color_scheme, custom
+  //     theme: 'light',
+  //     inputPosition: 'bottom',
+  //     lang: 'en',
+  //     darkTheme: 'dark',
+  //     themeURL: '',
+  //   },
+  // },
   socialAccount: {
     twitter: 'samekjordan',
   },
